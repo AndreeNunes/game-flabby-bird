bcc7d1f75fba26ca4fcc7f5860e354e9f2ff5ff1
André Nicola Nunes <57879149+AndreeNunes@users.noreply.github.com>
2021-05-19 13:32:35 -0300

    Animation of the Bird

--- core/src/com/andrez/flappybird/Game.java
+++ core/src/com/andrez/flappybird/Game.java
@@ -11,23 +11,38 @@ import javax.xml.soap.Text;
 public class Game extends ApplicationAdapter {
 
 	private SpriteBatch batch;
-	private Texture bird;
+	private Texture[] birds;
 	private Texture background;
 	private int moveX = 0;
+
+	private float widthDevice;
+	private float heightDevice;
+	private float variation = 0;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		bird = new Texture("passaro1.png");
+		birds = new Texture[3];
+		birds[0] = new Texture("passaro1.png");
+		birds[1] = new Texture("passaro2.png");
+		birds[2] = new Texture("passaro3.png");
 		background = new Texture("fundo.png");
+
+		widthDevice = Gdx.graphics.getWidth();
+		heightDevice = Gdx.graphics.getHeight();
 	}
 
 	@Override
 	public void render () {
 		batch.begin();
 
-		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(bird, moveX, 500);
+		if(variation > 3) variation = 0;
+
+		batch.draw(background, 0, 0, widthDevice, heightDevice);
+		batch.draw(birds[(int) variation], moveX, heightDevice / 2);
+
+		variation += Gdx.graphics.getDeltaTime() * 10;
+
 		moveX++;
 		batch.end();
 	}
